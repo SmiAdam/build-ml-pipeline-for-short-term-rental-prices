5bc9197b120fe0ee24107354343773273546a3ff
SmiAdam <adam.smietanka@gmail.com>
2024-02-09 11:28:10 +0100

    1.0.0 version

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+"""
+This steps cleans the data and saves results to Weights and Biases
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    
+    run = wandb.init(project="nyc_airbnb", group="basic_cleaning")
+
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+    df = pd.read_csv(artifact_path, low_memory=False)
+
+    logger.info("Data is filtered based on EDA step")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+
+    # Convert last_review to datetime
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    df.to_csv("clean_sample.csv", index=False)
+
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+    artifact.wait()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="This steps cleans the data")
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="",
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    go(args)
